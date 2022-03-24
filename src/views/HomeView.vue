10fcf4dc7c060ed0d19a444ed1ee5eb4110ec783
inincevic <inincevic@unipu.hr>
2022-03-24 14:08:51 +0100

    Added axios.get for all three links

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,55 @@
 <template>
-  <hello-world />
+  <v-form>
+    <v-container>
+      <v-text-field
+        v-model="message4"
+        label="Write your name here, please"
+        outlined
+        clearable
+      ></v-text-field>
+    </v-container>
+  </v-form>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
+import axios from 'axios';
 
-  export default {
-    name: 'Home',
+export default {
+  name: "Home",
 
-    components: {
-      HelloWorld,
+  data: () => ({
+    name: '',
+    age: null,
+    gender: null,
+    nationality: null
+  }),
+
+  methods: {
+    async agify (){
+       axios
+      .get('https://api.agify.io/?name=Ivan')
+      .then(response => (this.age = response))
+      console.log(this.age)
     },
+    async genderize (){
+       axios
+      .get('https://api.agify.io/?name=Ivan')
+      .then(response => (this.gender = response))
+      console.log(this.gender)
+    },
+    async nationalize (){
+       axios
+      .get('https://api.agify.io/?name=Ivan')
+      .then(response => (this.nationality = response))
+      console.log(this.nationality)
+    }
+  },
+
+  created() {
+    this.agify();
+    this.genderize();
+    this.nationalize();
   }
+
+};
 </script>
